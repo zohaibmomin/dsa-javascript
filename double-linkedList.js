17292dde80df92f692af115621cd97fe4ee486f4
zohaibmomin <zohaib.momin@kotak.com>
2023-05-08 21:11:50 +0530

    DLL- get a node

--- double-linkedList.js
+++ double-linkedList.js
@@ -52,14 +52,67 @@ class DoubleLinkedList {
             if (this.length === 1) {
                 this.tail = null;
                 this.head = null;
-            }else{
+            } else {
                 this.head = oldHead.next;
                 this.head.prev = null;
                 oldHead.next = null;
             }
-            
+
             this.length--;
             return this.oldHead;
         }
+
+    }
+    //adds node to start
+    unshift(val) {
+        let newNode = new Node(val);
+        if (!this.head) {
+            //if list was empty
+            this.head = newNode;
+            this.tail = newNode;
+            this.length++;
+        } else {
+            //if list is not empty
+            //link newNode next to currentHead
+            this.head.prev = newNode;
+            newNode.next = this.head;
+            this.head = newNode;
+            this.length++;
+        }
+        return this;
+    }
+
+    //get a node for a given index position
+    get(indexPos) {
+        //if indexPos is outside range
+        if (indexPos < 0 || indexPos >= this.length) {
+            return null
+        }
+        var current;
+        var counter;
+        if (indexPos <= this.length / 2) {
+            //element to the left
+            //start from the first
+            current = this.head;
+            counter = 0
+            //traverse till indexPos
+            while (counter !== indexPos) {
+                current = current.next;
+                counter++;
+            }
+
+        } else {
+            //element to the right
+            //start from the prev
+            current = this.tail;
+            counter = this.length - 1;
+            //traverse till indexPos
+            while (counter !== indexPos) {
+                current = current.prev;
+                counter--;
+            }
+        }
+
+        return current;
     }
 }
