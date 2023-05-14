40d693e17aec41c91bb7a32d469dc36ef620c371
zohaibmomin <zohaib.momin@kotak.com>
2023-05-14 12:41:36 +0530

    Stacks implementation

---
+++ stack.js
@@ -0,0 +1,38 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+class Stack {
+    constructor(val) {
+        this.first = null;
+        this.last = null;
+        this.size = 0;
+    }
+    /* Same as unshift of Single linked list - Add node to beginning*/
+    push(val) {
+        var newNode = new Node(val);
+        if (this.size === 0) {
+            this.first = newNode;
+            this.last = newNode;
+        } else {
+            var temp = this.first;
+            this.first = newNode;
+            this.first.next = temp;
+        }
+        return ++this.size;
+    }
+    /* Same as shift of Single Linked List - Remove beginning node */
+    pop() {
+        if (this.size === 0) return null;
+        var temp = this.first;
+        if (this.first === this.last) {
+            this.last = null;
+        }
+        this.first = this.first.next;
+        this.size--;
+        return temp;
+    }
+}
