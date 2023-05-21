eae08693025d02473583d4da29e66398f0f1a734
zohaibmomin <zohaibrulz@gmail.com>
2023-05-21 13:52:45 +0530

    BinaryHeap - Remove Max

--- maxBinaryHeap.js
+++ maxBinaryHeap.js
@@ -20,6 +20,65 @@ class MaxBinaryHeap {
             index = parentIndex;
         }
     }
+
+    extractMax() {
+        ////Process to Remove the root(max)
+        //swap first and last one(newly added node in the heap)
+        const max = this.values[0];
+        const end = this.values.pop;//pop will remove the last element and adjust the position also.
+        if (this.values > 0) {
+            this.values[0] = end;
+            this.sinkDown();
+        }
+        return max;
+
+    }
+
+    sinkDown() {
+        //Process to Remove the root(max)
+        //And then Find the correct position for this swapped node
+        //for this sink down with the largestChild
+
+        //First start with root node
+        let index = 0;
+        const element = this.values[0];
+
+        const length = this.values.length;
+
+        while (true) {
+            let leftChildIndex = 2 * index + 1;
+            let rightChildIndex = 2 * index + 2;
+            let leftChild, rightChild;
+            let swap = null;
+
+            if (leftChildIndex < length) {
+                leftChild = this.values[leftChildIndex];
+                //swap leftChild if bigger than root
+                if (leftChild > element) {
+                    swap = leftChildIndex;
+                }
+            }
+
+            if (rightChildIndex < length) {
+                rightChild = this.values[rightChildIndex];
+                //swap rightChild if bigger than root and also bigger than leftChild
+                if ((swap == null && rightChild > element) ||
+                    (swap !== null && rightChild > leftChild)
+                ) {
+                    swap = rightChildIndex;
+                }
+
+            }
+
+            if (swap === null) break;
+            //means swap has happened either with left or right child
+            //swap the new index ( started with index=0) with the swap index
+            this.values[index] = this.values[swap];
+            this.values[swap] = element;
+            index = swap;
+        }
+
+    }
 }
 
 var heap = new MaxBinaryHeap();
