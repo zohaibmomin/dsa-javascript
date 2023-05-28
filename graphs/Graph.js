7d61c1d5e0b4ecd4d47ef3fb083f7d35b4a27fbb
zohaibmomin <zohaibrulz@gmail.com>
2023-05-28 16:16:21 +0530

    DFS - recursive & iterative

--- graphs/Graph.js
+++ graphs/Graph.js
@@ -42,19 +42,104 @@ class Graph {
         */
         delete this.adjacencyList[vertex];
     }
+    dfsRecursive(start) {
+        let results = [];
+        let visited = {};
+
+        let adjacencyList = this.adjacencyList;
+        (function dfs(vertex) {
+            // Check if vertex exists
+            if (!vertex) return null;
+            // mark the vertex visited
+            visited[vertex] = true;
+            // push to results array
+            results.push(vertex);
+
+            //Go deep for all neighbours until deadend and then callback same dfs
+            adjacencyList[vertex].forEach(element => {
+                if (!visited[element]) {
+                    return dfs(element);
+                }
+            });
+        })(start)
+
+        return results;
+    }
+
+    dfsIterative(start) {
+        //maintain a stack with initialised as start node
+        let stack = [start];
+        let results = [];
+        let visited = {};
+
+        let currentVertex;
+        visited[start] = true;
+        while (stack.length) {
+            //remove from stack & push to results
+            currentVertex = stack.pop();
+            results.push(currentVertex);
+
+            // go deep for all neighbours
+            this.adjacencyList[currentVertex].forEach(element => {
+                // if not visited
+                // mark visit
+                // push to results
+                if (!visited[element]) {
+                    visited[element] = true;
+                    stack.push(element);
+                }
+
+            });
+
+
+
+        }
+        return results;
+
+
+
+    }
 }
 
+// let g = new Graph();
+// g.addVertex("Dallas");
+// g.addVertex("Tokyo");
+// g.addVertex("Aspen");
+// g.addVertex("Los Angeles");
+// g.addVertex("Hong Kong")
+// g.addEdge("Dallas", "Tokyo");
+// g.addEdge("Dallas", "Aspen");
+// g.addEdge("Hong Kong", "Tokyo");
+// g.addEdge("Hong Kong", "Dallas");
+// g.addEdge("Los Angeles", "Hong Kong");
+// g.addEdge("Los Angeles", "Aspen");
+
 let g = new Graph();
-g.addVertex("Dallas");
-g.addVertex("Tokyo");
-g.addVertex("Aspen");
-g.addVertex("Los Angeles");
-g.addVertex("Hong Kong")
-g.addEdge("Dallas", "Tokyo");
-g.addEdge("Dallas", "Aspen");
-g.addEdge("Hong Kong", "Tokyo");
-g.addEdge("Hong Kong", "Dallas");
-g.addEdge("Los Angeles", "Hong Kong");
-g.addEdge("Los Angeles", "Aspen");
+
+g.addVertex("A")
+g.addVertex("B")
+g.addVertex("C")
+g.addVertex("D")
+g.addVertex("E")
+g.addVertex("F")
+
+
+g.addEdge("A", "B")
+g.addEdge("A", "C")
+g.addEdge("B", "D")
+g.addEdge("C", "E")
+g.addEdge("D", "E")
+g.addEdge("D", "F")
+g.addEdge("E", "F")
+g.dfsRecursive("A")
+g.dfsIterative("A")
+
+//          A
+//        /   \
+//       B     C
+//       |     |
+//       D --- E
+//        \   /
+//          F
 
 
